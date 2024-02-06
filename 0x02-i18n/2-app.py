60315fdf9af1aa64b99bfed5f6f6ac3cd90ede7c
Prosper Mugisha <mprosp3r@gmail.com>
2024-02-07 01:45:39 +0200

    creation of 3-app file

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 """2-app"""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
-Config = __import__('config').Config
+# Config = __import__('config').Config
 
 
 app = Flask(__name__, template_folder='templates')
+
+
+class Config:
+    """this is the config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
@@ -15,7 +22,13 @@ babel = Babel(app)
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     """Basic Template for Babel Implementation"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match with our support language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == '__main__':
