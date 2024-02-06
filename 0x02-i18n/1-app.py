45752d64f29d056000ff6b2965f78993f2948cb5
Prosper Mugisha <mprosp3r@gmail.com>
2024-02-07 01:31:42 +0200

    creation of 2-app file

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""0-app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+Config = __import__('config').Config
+
+
+app = Flask(__name__, template_folder='templates')
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """Basic Template for Babel Implementation"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ 0x02-i18n/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""config file"""
+
+
+class Config:
+    """this is the config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
