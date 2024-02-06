c24e17ef513e5663e8779d5082da781805c2f5da
Prosper Mugisha <mprosp3r@gmail.com>
2024-02-07 01:37:17 +0200

    add missing class in 0-app file

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-"""0-app"""
+"""1-app"""
 from flask import Flask, render_template
 from flask_babel import Babel
-Config = __import__('config').Config
+# Config = __import__('config').Config
 
 
 app = Flask(__name__, template_folder='templates')
+
+
+class Config:
+    """this is the config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
